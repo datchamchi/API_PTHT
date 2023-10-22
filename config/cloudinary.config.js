1e5f5d31d469196e19d0bf758c743bdbcd78c11b
t_dat <vdat1608@gmail.com>
2023-10-23 00:40:56 +0700

    change api key

--- config/cloudinary.config.js
+++ config/cloudinary.config.js
@@ -5,16 +5,16 @@ import { CloudinaryStorage } from "multer-storage-cloudinary";
 import multer from "multer";
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDINARY_KEY,
-  api_secret: process.env.CLOUDINARY_SECRET,
+  cloud_name: "dwdveb6qp",
+  api_key: "657375929256455",
+  api_secret: "R9vIuAoS0PbpQPVIDKIWW2FNoaU",
 });
 
 const storage = new CloudinaryStorage({
   cloudinary,
   allowedFormats: ["jpg", "png"],
   params: {
-    folder: "imageMAU",
+    folder: "image",
   },
 });
 
